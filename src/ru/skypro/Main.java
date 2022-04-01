190a50941f851fdd9082fb693f73299b816b7363
vivatgreen <grindsasha@bk.ru>
2022-04-01 23:45:03 +0300

    ДЗ

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,17 +3,19 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        Author stephenKing  = new Author("Stephen", "King");
-        Book firstBook = new Book("Desperation", stephenKing, 1997);
+        Author stephenKing  = new Author ("Stephen", "King");
+        Book firstBook = new Book ("Desperation", stephenKing, 1997);
 
         Author levTolstoi  = new Author("Толстой", "Лев");
         Book secondBook = new Book("Война и мир", levTolstoi, 1869);
 
-        System.out.println(firstBook.getName() + " " + firstBook.getYear()+ " " + firstBook.getAuthor());
+        System.out.println (firstBook.getName() + " " + firstBook.getYear()+ " " + firstBook.getAuthor());
+        System.out.println (secondBook.getName() + " " + secondBook.getYear()+ " " + secondBook.getAuthor());
 
-        System.out.println("firstBook = " + firstBook);
-        System.out.println("secondBook = " + secondBook);
-        secondBook.setYear(1846);
-        System.out.println("secondBook.getYear() = " + secondBook.getYear());
+
+        System.out.println ("firstBook = " + firstBook);
+        System.out.println ("secondBook = " + secondBook);
+        secondBook.setYear (1846);
+        System.out.println ("secondBook.getYear() = " + secondBook.getYear());
     }
     }
